f03b2c226891adaa34874e43c1fad3a2d7b0dadd
Amandeep <amankumar283@gmail.com>
2023-06-08 14:32:34 -0400

    login done

--- app/src/main/java/com/amandeep/sqliteloginsignup/LoginActivity.java
+++ app/src/main/java/com/amandeep/sqliteloginsignup/LoginActivity.java
@@ -3,23 +3,23 @@ package com.amandeep.sqliteloginsignup;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignupActivity extends AppCompatActivity {
-    TextView bottom_login;
+public class LoginActivity extends AppCompatActivity {
+    TextView signup_text;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
-        bottom_login=findViewById(R.id.signup_bottom);
-        bottom_login.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.login_activity);
+        signup_text=findViewById(R.id.ls_bottom);
+        signup_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(SignupActivity.this,LoginActivity.class);
+                Intent intent=new Intent(LoginActivity.this,SignupActivity.class);
                 startActivity(intent);
             }
         });
